7d0ec9e7507fc9e44ec27b9fe68e69bb8ff08a64
david <smashyalts@gmail.com>
2023-02-02 14:55:55 +0100

    Works, to claim the weapon, shift right-click, u have to be in creative to claim the axe

--- src/main/java/guiexamples/guiexamples/GuiEventsMain/Guievents.java
+++ src/main/java/guiexamples/guiexamples/GuiEventsMain/Guievents.java
@@ -1,10 +1,13 @@
 package guiexamples.guiexamples.GuiEventsMain;
 
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -13,13 +16,18 @@ import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
+import org.bukkit.plugin.RegisteredServiceProvider;
+import guiexamples.guiexamples.*;
 import java.util.Collections;
 
+import static guiexamples.guiexamples.Guiexamples.econ;
+import static jdk.internal.org.jline.utils.Colors.s;
+import static org.bukkit.Bukkit.getServer;
 import static org.bukkit.Material.DIAMOND_AXE;
 
 public class Guievents implements Listener {
     private Inventory Inv;
+
     public ItemStack GuiItems;
 
     public void InitializeItems () {
@@ -32,7 +40,9 @@ public class Guievents implements Listener {
 }
     @EventHandler
     public void onGuiOpen (PlayerInteractEvent e) {
-        if (e.getPlayer().isSneaking() && e.getHand() == (EquipmentSlot.HAND) && e.getAction().isRightClick()) {
+        //EconomyResponse r = econ.depositPlayer(player, 1.05);
+
+        if (e.getPlayer().isSneaking() && e.getHand() == (EquipmentSlot.HAND)) {
             Inv = Bukkit.createInventory(null, 9, ChatColor.DARK_AQUA + "Death Merchant");
             InitializeItems();
             Inv.setItem(4, GuiItems);
@@ -47,9 +57,15 @@ public class Guievents implements Listener {
     public void onInventory (InventoryClickEvent e) {
         if (e.getClickedInventory() == (Inv)){
             e.setCancelled(true);
-            if (e.getWhoClicked().getGameMode() == (GameMode.CREATIVE)) {
-                e.getWhoClicked().sendMessage("You have been deemed worthy of claiming the" + ChatColor.DARK_RED + " " + "Destroyer Of Gods");
+            Player player = (Player) e.getWhoClicked();
+            EconomyResponse r = econ.withdrawPlayer(player, 100);
+            if (r.transactionSuccess()) {
+                e.getWhoClicked().sendMessage("Your wealth has been deemed worthy of claiming the" + ChatColor.DARK_RED + " " + "Destroyer Of Gods");
                 e.setCancelled(false);
+            }
+            else if (r.transactionSuccess() == false){
+                e.getWhoClicked().sendMessage(ChatColor.RED + "Your wealth has been deemed unworthy of claiming the" + ChatColor.DARK_RED + " " + "Destroyer Of Gods");
+                e.getWhoClicked().sendMessage(ChatColor.RED + "You have" + " " + ChatColor.AQUA + econ.format(r.balance) + ChatColor.RED + " " + "and u need" + " " + ChatColor.DARK_AQUA + (100 - r.balance) + ChatColor.RED + " " + "to be deemed worthy of the" + ChatColor.DARK_RED + " " + "Destroyer Of Gods");
             }
 
         }
--- src/main/java/guiexamples/guiexamples/Guiexamples.java
+++ src/main/java/guiexamples/guiexamples/Guiexamples.java
@@ -1,20 +1,38 @@
 package guiexamples.guiexamples;
 
 import guiexamples.guiexamples.GuiEventsMain.Guievents;
-import org.bukkit.inventory.Inventory;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Guiexamples extends JavaPlugin {
-
+    public static Economy econ = null;
     @Override
     public void onEnable() {
         // Plugin startup logic
         this.getServer().getPluginManager().registerEvents(new Guievents(), this);
-
+        if (!setupEconomy() ) {
+            this.getServer().getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+    }}
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
     }
-
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static Economy getEconomy() {
+        return econ;
     }
 }
